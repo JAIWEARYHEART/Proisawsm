8d77c7c7217d9a5dee504c83cafc7e276043fe83
VINAYJAISWAL <vinayjaiswal22898@gmail.com>
2017-12-24 09:45:20 -0700

    id has added

--- Student.java
+++ Student.java
@@ -1,6 +1,6 @@
 class Student
 {
-      int rollno,marks;
+      int rollno,marks, id;
       void rollno(int rollno)
       {
         this.rollno=rollno;
@@ -9,12 +9,17 @@ class Student
       {
        this.marks=marks;
       }
+      void id(int id)
+      {
+        this.id=id;
+      }
    public static void main(String s[])
    {
      Student st;
      st=new Student();
      st.rollno(110);
      st.marks(50);
+     st.id(100)    
    }
 
 }
